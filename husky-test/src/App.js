1fed8f489669a7decd42008a9c327b24e3e81cd9
RotcehRuiz <ruiz.hectoralum023@gmail.com>
2024-12-15 15:14:02 -0600

    Testing Husky and lint-staged

--- husky-test/src/App.js
+++ husky-test/src/App.js
@@ -1,7 +1,8 @@
 function App() 
   console.log("This is not formatted correctly");
   return <div>App</div>;
-  
+  const unusedVariable = 123
+console.log("Double quotes are bad");
   {}
   export default App;
   const unusedVariable = 123
